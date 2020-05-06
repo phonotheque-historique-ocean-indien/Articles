ed9211194bd8b21547b83ac835a9172877f2178b
Rachel <ro@ideesculture.com>
2020-05-06 11:49:25 +0200

    fix lead_dropcap and link to article

--- views/home_block_html.php
+++ views/home_block_html.php
@@ -1,11 +1,20 @@
 <?php
 $article = $this->getVar("article");
-//echo "ok" .is_array($article["blocs"]) . "<br>"; pour vérifier qu'il s'agit d'un array
-echo '<pre>';
-print_r($article["blocs"]);
-//var_dump($article["blocs"]);
-echo '<pre>';
-//echo $article["blocs"];
+$id = $this->getVar("id");
+////echo "ok" .is_array($article["blocs"]) . "<br>"; pour vérifier qu'il s'agit d'un array
+//echo '<pre>';
+//print_r($article["blocs"]);
+////var_dump($article["blocs"]);
+//echo '<pre>';
+////echo $article["blocs"];
+$article["blocs"]=str_replace('\\\n',"",$article["blocs"]);
+$blocs=json_decode($article["blocs"],true);
+//var_dump($blocs);die();
+$content= $blocs[1]["content"];
+$content=mb_substr($content,0,119);
+if(mb_strlen($content)==119) {
+    $content=$content."...";
+}
 ?>
 <div class="column is-one-third">
     <div class="card">
@@ -15,19 +24,19 @@ echo '<pre>';
             </figure>
         </div>
         <div class="card-content">
-            <div class="content">
+            <div class="content" style="height:210px">
                 <div class="card-details">
                     <p class="author"><?php _p($article["author"]); ?></p>
                     <p class="date pull-right"><?php _p($article["date"]); ?></p>
                 </div>
                 <h2 class="card-title"><?php _p($article["title"]); ?></h2>
                 <h3 class="card-subtitle"><?php _p($article["subtitle"]); ?></h3>
-                <p></p>
+                <p><?php _p($content); ?></p>
             </div>
         </div>
         <footer class="card-footer">
-            <a href="./Details/id/1" class="card-footer-item">Lire l'article</a>
-<!--            <a href="./Details/id/--><?php //_p($article["id"]); ?><!--" class="card-footer-item">Lire l'article </a>-->
+<!--            <a href="./Details/id/1" class="card-footer-item">Lire l'article</a>-->
+            <a href="./Details/id/<?php _p($id); ?>" class="card-footer-item">Lire l'article </a>
         </footer>
     </div>
 </div>
