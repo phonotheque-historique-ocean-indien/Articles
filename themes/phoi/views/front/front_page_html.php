6e577f9d905153c026a40025bbd1830c1dcbb3f5
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-01 15:59:16 +0200

    front page playlists

--- themes/phoi/views/front/front_page_html.php
+++ themes/phoi/views/front/front_page_html.php
@@ -20,10 +20,100 @@
 </style>
 </div>
 <div>
-<div style="background-color:#f7f6f7">
+<div style="background-color:#f7f6f7;padding-bottom:70px;">
     <div class="container">
         <h2>A l'écoute</h2>
-        <table class="table dataTable" id="revueDePresse" role="grid" style="background-color: transparent">
+        <div id="is-playing">
+            <div class="gwrapper is-playing">
+                <article class="gmain">
+                    <div class="columns is-centered">
+                        <div class="column text-center">
+                            <i class="mdi mdi-skip-previous is-large" style="font-size:2.6em;"></i>
+                        </div>
+                        <div class="column text-center">
+                            <i class="mdi mdi-pause-circle is-large" style="font-size:2.6em;"></i>
+                        </div>
+                        <div class="column text-center">
+                            <i class="mdi mdi-skip-next is-large" style="font-size:2.6em;"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="pull-left">01:43</div>
+                        <div class="pull-right">-01:57</div>
+                    </div>
+                    <progress class="progress progress4px is-danger" value="65" max="100">15%</progress>
+                </article>
+                <aside class="gaside aside-1" style="text-align: left;"><img src="https://phoi.ideesculture.fr/phoi-images/podcasts/Danyel_waro_3.jpeg" style="height:120px;width:auto;"/>
+                    <p><b>Panier su la tête, ni chanté</b></p>
+                    <p>Alain Peters</p>
+                </aside>
+                <aside class="gaside aside-2 has-text-left">
+                    <div class="columns is-centered">
+                        <div class="column has-text-left">
+                        <i class="mdi mdi-volume-high is-large" style="font-size:2.6em;"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="pull-left">&nbsp;</div>
+                        <div class="pull-right">&nbsp;</div>
+                    </div>
+
+                    </p>
+                    <progress class="progress progress4px" value="15" max="100">15%</progress>
+
+                </aside>
+            </div>
+        <style>
+            .gwrapper.is-playing {
+                display: flex;
+                flex-flow: row wrap;
+                font-weight: normal;
+                text-align: center;
+            }
+            .gwrapper.is-playing img {
+                float:left;
+                margin-right:20px;
+            }
+
+            .gwrapper > * {
+                padding: 10px;
+                flex: 1 100%;
+            }
+
+            .gmain {
+                padding-left:0px;
+                padding-right: 160px;
+            }
+
+            .aside-1.gaside { flex: 3 0 0; }
+            .aside-1.gaside p:first-of-type {
+                margin-top:12px;
+            }
+
+            .aside-2 {
+            }
+
+            @media all and (min-width: 600px) {
+                .gaside { flex: 1 0 0; }
+            }
+
+            @media all and (min-width: 800px) {
+                .gmain    { flex: 3 0px; }
+                .aside-1 { order: 1; }
+                .gmain    { order: 2; }
+                .aside-2 { order: 3; }
+                .gfooter  { order: 4; }
+            }
+            .progress.progress4px {
+                height:4px;
+            }
+            .is-playing .column {
+                padding-bottom:0;
+                padding-top:24px;
+            }
+        </style>
+        </div>
+        <table class="table dataTable" id="alecoute" role="grid" style="background-color: transparent">
             <thead>
             <tr role="row">
                 <th class="sorting_asc" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Titre: activer pour trier la colonne par ordre décroissant" style="width: 903px;">Album</th>
@@ -61,10 +151,10 @@
 
 </div>
 
-<div style="height:400px; background-color:#f2f2f2">
+<div style="background-color:#f2f2f2">
     <div class="container">
-        <h2>A l'écoute</h2>
-        <table class="table dataTable" id="revueDePresse" role="grid" style="background-color: transparent">
+        <h2>Dernières playlists</h2>
+        <table class="table dataTable" id="lastplaylists" role="grid" style="background-color: transparent">
             <thead>
             <tr role="row">
                 <th class="sorting_asc" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Titre: activer pour trier la colonne par ordre décroissant" style="width: 903px;">Album</th>
@@ -98,6 +188,10 @@
                 <td>Julien LG</td>
             </tr></tbody>
         </table>
+        <div style="text-align:center;padding:4px 0 40px 0;">
+            <button class="button is-primary" style="background-color: #e4675f;padding:12px 70px;font-size:1.3em;">Voir plus de playlists</button>
+        </div>
     </div>
 </div>
+
     <div>
