a92393a80796058f42d716ac299852bd1f66c769
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-01 10:53:30 +0200

    home : work in progress

--- themes/phoi/views/front/front_page_html.php
+++ themes/phoi/views/front/front_page_html.php
@@ -20,10 +20,84 @@
 </style>
 </div>
 <div>
-<div style="height:400px; background-color:#f7f6f7">
-    A l'écoute
+<div style="background-color:#f7f6f7">
+    <div class="container">
+        <h2>A l'écoute</h2>
+        <table class="table dataTable" id="revueDePresse" role="grid" style="background-color: transparent">
+            <thead>
+            <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Titre: activer pour trier la colonne par ordre décroissant" style="width: 903px;">Album</th>
+                <th class="sorting" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-label="Date: activer pour trier la colonne par ordre croissant" style="width: 147px;">Artiste</th>
+                <th class="sorting" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-label="Auteur: activer pour trier la colonne par ordre croissant" style="width: 207px;">Auteur</th></tr>
+            </thead>
+            <tbody>
+            <tr role="row" class="odd">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/5">Abdullah Ibrahim, la spiritualité faite musique</a></td>
+                <td>23.12.2018</td>
+                <td>Jonathan GRONDIN</td>
+            </tr><tr role="row" class="even">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/1">Alain Peters Rest’ la maloya</a></td>
+                <td>12.12.2019</td>
+                <td>Jonhatan Grondin</td>
+            </tr><tr role="row" class="odd">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/4">Dj Sebb, le maître réunionnais de la Gommance</a></td>
+                <td>04.05.2020</td>
+                <td>Jonhatan Grondin</td>
+            </tr><tr role="row" class="even">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/2">Le séga réunionnais inscrit à l'inventaire national du Patrimoine Culturel Immatériel de France</a></td>
+                <td>JJ.MM.AAAA</td>
+                <td>Jonhatan Grondin</td>
+            </tr><tr role="row" class="odd">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/6">Pendant le IOMMA,  les femmes donnent le la !</a></td>
+                <td>30.04.2020</td>
+                <td>Jonathan GRONDIN</td>
+            </tr><tr role="row" class="even">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/3">Siti &amp; The Band Le pouvoir aux femmes</a></td>
+                <td>28/04/2020</td>
+                <td>Julien LG</td>
+            </tr></tbody>
+        </table>
+    </div>
+
 </div>
 
 <div style="height:400px; background-color:#f2f2f2">
-    A l'écoute
+    <div class="container">
+        <h2>A l'écoute</h2>
+        <table class="table dataTable" id="revueDePresse" role="grid" style="background-color: transparent">
+            <thead>
+            <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Titre: activer pour trier la colonne par ordre décroissant" style="width: 903px;">Album</th>
+                <th class="sorting" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-label="Date: activer pour trier la colonne par ordre croissant" style="width: 147px;">Artiste</th>
+                <th class="sorting" tabindex="0" aria-controls="revueDePresse" rowspan="1" colspan="1" aria-label="Auteur: activer pour trier la colonne par ordre croissant" style="width: 207px;">Auteur</th></tr>
+            </thead>
+            <tbody>
+            <tr role="row" class="odd">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/5">Abdullah Ibrahim, la spiritualité faite musique</a></td>
+                <td>23.12.2018</td>
+                <td>Jonathan GRONDIN</td>
+            </tr><tr role="row" class="even">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/1">Alain Peters Rest’ la maloya</a></td>
+                <td>12.12.2019</td>
+                <td>Jonhatan Grondin</td>
+            </tr><tr role="row" class="odd">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/4">Dj Sebb, le maître réunionnais de la Gommance</a></td>
+                <td>04.05.2020</td>
+                <td>Jonhatan Grondin</td>
+            </tr><tr role="row" class="even">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/2">Le séga réunionnais inscrit à l'inventaire national du Patrimoine Culturel Immatériel de France</a></td>
+                <td>JJ.MM.AAAA</td>
+                <td>Jonhatan Grondin</td>
+            </tr><tr role="row" class="odd">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/6">Pendant le IOMMA,  les femmes donnent le la !</a></td>
+                <td>30.04.2020</td>
+                <td>Jonathan GRONDIN</td>
+            </tr><tr role="row" class="even">
+                <td class="sorting_1"><a href="/index.php/Articles/Show/Details/id/3">Siti &amp; The Band Le pouvoir aux femmes</a></td>
+                <td>28/04/2020</td>
+                <td>Julien LG</td>
+            </tr></tbody>
+        </table>
+    </div>
 </div>
+    <div>
