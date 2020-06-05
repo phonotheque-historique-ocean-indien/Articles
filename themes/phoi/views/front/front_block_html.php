d295f28783267dec512858e39f6c3e4a149b9680
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-05 21:01:27 +0200

    Uppercasing the template in the URL

--- themes/phoi/views/front/front_block_html.php
+++ themes/phoi/views/front/front_block_html.php
@@ -29,7 +29,7 @@ if(mb_strlen($content)==119) {
             </div>
         </div>
         <footer class="card-footer">
-            <a href="<?php _p(__CA_URL_ROOT__."/index.php/".$template_title."s/Show/Details/id/".$id); ?>" class="card-footer-item">Lire l'article </a>
+            <a href="<?php _p(__CA_URL_ROOT__."/index.php/".ucfirst($template_title)."s/Show/Details/id/".$id); ?>" class="card-footer-item">Lire l'article </a>
         </footer>
     </div>
 </div>
