08d37998095f327364e0b2687c38039116fcb6b6
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-05 21:01:37 +0200

    relative links

--- themes/phoi/views/front/front_page_html.php
+++ themes/phoi/views/front/front_page_html.php
@@ -18,8 +18,8 @@
 </style>
 </div>
 <div style="background-color: #f2f2f2;margin-top:80px;">
-<iframe id="audio-player" style="width:100%;height:700px;background-color: #f7f6f7;margin:0;padding:0;" src="http://phoi.ideesculture.test/index.php/AudioPlayer/v/Embed">
-</iframe><iframe id="audio-player" style="width:100%;height:500px;background-color: #f2f2f2;margin:0;padding:0;" src="http://phoi.ideesculture.test/index.php/AudioPlayer/v/LastPlaylists">
+<iframe id="audio-player" style="width:100%;height:700px;background-color: #f7f6f7;margin:0;padding:0;" src="/index.php/AudioPlayer/V/Embed">
+</iframe><iframe id="audio-player" style="width:100%;height:500px;background-color: #f2f2f2;margin:0;padding:0;" src="/index.php/AudioPlayer/V/LastPlaylists">
 </iframe>
 
     <div>
