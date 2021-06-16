77106502dab76a457c81f1e4911746b85061bc45
Gautier Michelin <gm@ideesculture.com>
2021-06-16 22:19:04 +0400

    clickable card, case insensitive URLs

--- themes/phoi/views/front/front_block_html.php
+++ themes/phoi/views/front/front_block_html.php
@@ -12,14 +12,22 @@ if(mb_strlen($content)==119) {
     $content=$content."...";
 }
 ?>
-<div class="column is-one-third">
+<div class="column is-one-third home-articles">
     <div class="card">
-        <div class="card-image">
+        <div class="card-image" onClick='window.location.href = "<?php _p(__CA_URL_ROOT__."/index.php/".ucfirst($template_title)."s/Show/Details/id/".$id); ?>";return false;'>
             <figure class="image is-3by2">
-                <img src="<?php _p($article["image"]); ?>" alt="image thumbnail">
+                <?php 
+                	$article["image"] = str_replace("https://phoi.ideesculture.fr/", "/", $article["image"]);
+                    if($article["image"]):
+                ?>
+                <img src="<?= $article["image"] ?>" alt="image thumbnail">
+                <?php else: ?>
+                    <img style="background: linear-gradient(135deg, rgba(23,87,139,1) 0%, rgba(50,138,173,1) 35%, rgba(124,175,201,1) 100%);">
+                <?php endif; ?>
             </figure>
+
         </div>
-        <div class="card-content">
+        <div class="card-content" onClick=' window.location.href = "<?php _p(__CA_URL_ROOT__."/index.php/".ucfirst($template_title)."s/Show/Details/id/".$id); ?>";return false;'>
             <div class="content">
                 <span class="tag is-primary <?php _p($categ); ?>"><?php _p($categ); ?></span>
                 <div class="pull-right"><?php _p($article["date"]); ?></div>
@@ -29,7 +37,7 @@ if(mb_strlen($content)==119) {
             </div>
         </div>
         <footer class="card-footer">
-            <a href="<?php _p(__CA_URL_ROOT__."/index.php/".$template_title."s/Show/Details/id/".$id); ?>" class="card-footer-item">Lire l'article </a>
+            <a href="<?php _p(__CA_URL_ROOT__."/index.php/".ucfirst($template_title)."s/Show/Details/id/".$id); ?>" class="card-footer-item">Lire l'article </a>
         </footer>
     </div>
 </div>
