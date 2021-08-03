a390e762dba99cea8761e20918f7988bc4c91a54
Gautier Michelin <gm@ideesculture.com>
2021-08-03 07:34:30 +0200

    display in 5 columns for set content

---
+++ themes/phoi/views/display/set_html.php
@@ -0,0 +1,56 @@
+<?php
+    $id = $this->getVar("id");
+?>
+<head>
+    <title>CollectiveAccess</title>
+    <link rel='stylesheet' href='/themes/phoi/assets/pawtucket/css/theme.css' type='text/css' media='all'/>
+</head>
+<body>
+    <div class="columns is-mobile">
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+    </div>
+    <div class="columns is-mobile">
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+        <div class="column">
+            <img src="https://via.placeholder.com/250x264" />
+        </div>
+    </div>
+<style>
+    html {
+        background-color: #eee;
+        padding:10px;
+        overflow: hidden;
+    }
+    .title {
+
+    }
+    .subtitle {
+        font-weight: 200;
+    }
+</style>
+</body>
